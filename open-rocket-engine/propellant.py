c113bd464d52aca91fea8c9ec41541f6f954de7e
Cameron Flannery <cflannery@Camerons-MacBook-Pro.local>
2016-12-25 11:56:23 -0800

    updating propellant methods

--- open-rocket-engine/propellant.py
+++ open-rocket-engine/propellant.py
@@ -2,21 +2,24 @@ import os
 import json
 
 
-with open('propellant.json') as propellant_combinations:
-    d = json.load(propellant_combinations)
-    propellant_combinations.close()
-    print d
+with open('propellant.json') as propellant_raw:
+    propellant_data = json.load(propellant_raw)
+    propellant_raw.close()
 
 
+# prompt_for_propellants():
+#   prompt user for propellants
 def prompt_for_propellants():
     confirmed = False
+    propellants = []
     while not(confirmed):
-        Oxidizer = raw_input("Enter oxidizer in atomic forumula, i.e. O2: ")
-        Fuel = raw_input("Enter fuel in atomic formula/shorthand, i.e. RP1: ")
+        propellants = []
+        propellants.append(raw_input("Enter oxidizer in atomic forumula, i.e. O2: "))
+        propellants.append(raw_input("Enter fuel in atomic formula/shorthand, i.e. RP1: "))
         print "Oxidizer: ",
-        print Oxidizer
+        print propellants[0]
         print "Fuel: ",
-        print Fuel
+        print propellants[1]
         while True:
             response = raw_input("Correct? [Y/n]: ")
             if response.upper() == 'Y':
@@ -27,8 +30,24 @@ def prompt_for_propellants():
                 break
             else:
                 print "Please enter Y or n"
-    return 0
+    return propellants
+
+
+# pull_Isp(propellants[], propellant_data[])
+#   propellants[0] : Oxidizer
+#   propellants[1] : Fuel
+def pull_Isp(propellants, propellant_data):
+    return propellant_data["Propellants_Dict"]
+
+
+# main()
+#   used for testing and debugging
+#   only executes when main script
+def main():
+    propellants = prompt_for_propellants()
+    Isp = pull_Isp(propellants, propellant_data)
+    print Isp
 
 
 if __name__ == "__main__":
-    prompt_for_propellants()
+    main()
