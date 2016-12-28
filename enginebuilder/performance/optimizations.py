4e7d675e01c40c0240d4b654ccb283088413f20d
cflannery <cmflannery@ucsd.edu>
2016-12-28 13:05:40 -0800

    initial commit of new file

---
+++ enginebuilder/performance/optimizations.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import numpy as np
+# optimizations.py
+__author__ = "Cameron Flannery"
+__copyright__ = "Copyright 2016"
+__credits__ = ["Cameron Flannery"]
+__license__ = "MIT"
+__version__ = "1.0"
+__maintainer__ = "Cameron Flannery"
+__email__ = "cmflannery@ucsd.edu"
+__status__ = "Development"
