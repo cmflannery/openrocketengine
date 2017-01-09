f391cdabf21f71c2fefb1e74c79b8ac490884b6b
cflannery <cmflannery@ucsd.edu>
2017-01-09 13:36:17 -0800

    updating nozzle equations

--- enginebuilder/nozzle/nozzle.py
+++ enginebuilder/nozzle/nozzle.py
@@ -24,31 +24,68 @@ class nozzle(object):
         self.Aexit = self.get_Aexit()
         self.Vchamber = self.get_Vchamber()
         self.Aachamber = self.get_Achamber()
+        self.L = self.get_L()
 
     # Athroat (nozzle throat area)
     #   expects pchamber in atm
     def get_Athroat(self):
+        """ returns cros-sectional area of throat """
         return (self.performance.wdot*self.parameters.Isp) / \
-                (self.parameters.pchamber*self.performance.Cf)
+               (self.parameters.pchamber*self.performance.Cf)
 
     def get_Dthroat(self):
+        """ returns diameter of throat """
         return np.sqrt(self.Athroat * 4.0 / np.pi)
 
-    def get_Ln(self):
-        return -1
+    def get_Rthroat(self):
+        """ returns radius of throat """
+        return self.Dthroat / 2.0
 
     def get_Aexit(self):
+        """ returns exit area """
         return self.performance.epsilon * self.Athroat
 
     def get_Vchamber(self):
+        """ returns volume of the chamber """
         return self.parameters.L_star * self.Athroat
 
     def get_Achamber(self):
+        """ returns cros-sectional area of the chamber """
         return -1
 
+    def get_L(self):
+        """ returns length of 15 degree conic based on expansion ratio """
+        return ((np.sqrt(self.performance.epsilon) * self.Dthroat) -
+                self.Dthroat) / np.tan(15.00/180.00*np.pi)
 
-class _80deg_bell(object):
+
+class bell(object):
     """Define dimensions and parameters for an 80deg bell nozzle."""
-    def __init__(self):
-        # code here
+    def __init__(self, nozzle_obj, bell_percent):
+        self.nozzle_obj = nozzle_obj
+        self.bell_percent = bell_percent
+        pass
+
+    def get_Lbell(self):
+        """ return bell length """
+        return -1
+
+    def before_throat(self):
+        """ returns radius of curvature before throat """
+        ratio = 1.5  # see Huzel and Huang
+        self.D11 = nozzle_obj.Dthroat * ratio
+
+    def after_throat(self):
+        """ returns radius of curvature after throat """
+        ratio = 0.382
+        self.D12 = nozzle_obj.Dthroat * ratio
+
+    def get_N():
+        pass
+
+
+class conic(object):
+    """docstring for conic."""
+    def __init__(self, arg):
+        super(conic, self).__init__()
         pass
