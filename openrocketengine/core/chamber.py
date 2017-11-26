06ad60c9bbc1920a2ccb5b87d9442d43df527f21
Cameron Flannery <cflannery@Camerons-MacBook-Pro.local>
2017-11-26 00:58:16 -0800

    removing check for variable

--- openrocketengine/core/chamber.py
+++ openrocketengine/core/chamber.py
@@ -1,25 +1,24 @@
 from __future__ import division, absolute_import, print_function
 
 
-class chamber():
+class Chamber():
     def __init__(self, thermo):
         self.thermo = thermo
         pass
 
     def get_At(self, **kwargs):
         """ get_At returns the calculated area of the throat """
-        if self.At not in locals():
-            if 'mdot' in kwargs:
-                mdot = kwargs['mdot']
-            else:
-                mdot = thermo['mdot']
-            if 'cstar' in kwargs:
-                cstar = kwargs['cstar']
-            else:
-                cstar = thermo['cstar']
-            if 'pc' in kwargs:
-                pc = kwargs['pc']
-            else:
-                pc = thermo['pc']
-            self.At = cstar * mdot / pc
+        if 'mdot' in kwargs:
+            mdot = kwargs['mdot']
+        else:
+            mdot = self.thermo['mdot']
+        if 'cstar' in kwargs:
+            cstar = kwargs['cstar']
+        else:
+            cstar = self.thermo['cstar']
+        if 'pc' in kwargs:
+            pc = kwargs['pc']
+        else:
+            pc = self.thermo['pc']
+        self.At = cstar * mdot / pc
         return self.At
