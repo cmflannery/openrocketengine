e4a843d6f13abca882772585abd3e5a062b1389e
Cameron Flannery <cflannery@Camerons-MacBook-Pro.local>
2017-11-26 01:21:05 -0800

    adding more functionality and tests

--- openrocketengine/core/chamber.py
+++ openrocketengine/core/chamber.py
@@ -22,3 +22,30 @@ class Chamber():
             pc = self.thermo['pc']
         self.At = cstar * mdot / pc
         return self.At
+
+    def get_Ae(self, **kwargs):
+        """ get_Ae returns the exit area of the rocket, assuming ideal expansion
+        in a non-vacuum environment """
+        return self.calc_A(Ma=self.Ma_exit)
+
+    def calc_A(self, **kwargs):
+        """ calc_A returns the area at an arbitrary station relative to the
+        critical value, i.e. the throat in choked flow
+
+        calc_A does not set any property of the class or class instance, only
+        a value is returned """
+        if 'Ma' not in kwargs:
+            print('Mach number must be specified when calling calc_A')
+            return 0
+        Ma = kwargs['Ma']
+
+        if 'At' in kwargs:
+            At = kwargs['At']
+        else:
+            At = self.At
+        if 'gamma' in kwargs:
+            gamma = kwargs['gamma']
+        else:
+            gamma = self.thermo['gamma']
+
+        return At*1/Ma*((1 + ((gamma-1)/2)*Ma**2)/(1 + ((gamma-1)/2)))**((gamma+1)/(2*(gamma-1)))
--- openrocketengine/tests/test_chamber.py
+++ openrocketengine/tests/test_chamber.py
@@ -3,5 +3,10 @@ import openrocketengine as ore
 
 
 def test_Chamber():
-    chamber_test = ore.Chamber(1)
-    assert chamber_test.get_At(mdot=2.3,cstar=5400,pc=3000) == 4.14
+    thermo = {
+            'gamma': 1.27
+              }
+    chamber = ore.Chamber(thermo)
+    assert chamber.get_At(mdot=2.3,cstar=5400,pc=3000) == 4.14
+    assert chamber.calc_A() == 0
+    assert chamber.calc_A(Ma=2) == 7.46561495970419
